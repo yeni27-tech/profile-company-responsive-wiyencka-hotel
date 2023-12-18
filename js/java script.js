d2fb4385ec57a20c4f7160da56d080ab38dd5e92
yeni27-tech <yenifaturohmah2@gmail.com>
2023-12-18 15:45:01 +0700

    project

---
+++ js/java script.js
@@ -0,0 +1,38 @@
+//navbar//
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 200) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
+
+//drop down//
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll("li");
+const highlight = document.querySelector(".highlight");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  if (this.dataset.open === "close") {
+    menu.style.clipPath = "circle(100% at 50% 50%)";
+    this.dataset.open = "open";
+  } else {
+    menu.style.clipPath = "";
+    this.dataset.open = "close";
+    highlight.style = "";
+  }
+});
+
+links.forEach((link) => {
+  link.addEventListener("pointerover", function () {
+    const w = this.offsetWidth,
+      h = this.offsetHeight,
+      t = this.offsetTop;
+    highlight.style.cssText = `transform: translateY(${t}px); width: ${w}px; height: ${h}px`;
+  });
+});
