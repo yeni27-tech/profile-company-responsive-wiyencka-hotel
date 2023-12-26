4be7501b124e739cd7d73bce7f55e8837ac5b31e
yeni27-tech <yenifaturohmah2@gmail.com>
2023-12-26 11:57:07 +0700

    propt

--- js/java script.js
+++ js/java script.js
@@ -183,3 +183,30 @@ function slideIndicator(toIndex) {
       <p><strong>Subtotal:</strong>Rp  ${orderDetails.subtotal.toFixed(2)}</p>
     `;
     //selesai//
+
+
+
+    //fasilities//
+    let btns = document.querySelectorAll(".btn");
+    let banner = document.getElementById("banner");
+
+btns.forEach((btn, index) => {
+btn.addEventListener("click", () => {
+    banner.src = `../images/${index}.jpg`;
+    animation();
+    btn.classList.add("active");
+  });
+});
+
+function animation() {
+  banner.classList.add("zoom");
+
+  setTimeout(() => {
+    banner.classList.remove("zoom");
+  }, 1000);
+
+  for (b of btns) {
+    b.classList.remove("active");
+  }
+}
+//selesai//
